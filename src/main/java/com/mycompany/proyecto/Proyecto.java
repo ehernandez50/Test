5a04cf74c559e80635c4a388d7116d7cd3e778c8
jovany <jovany@Jovany>
2025-03-03 19:15:41 -0600

    commit

---
+++ src/main/java/com/mycompany/proyecto/Proyecto.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.proyecto;
+
+public class Proyecto {
+
+    public static void main(String[] args) {
+     
+        
+        
+        
+        for (int i = 1; i < 100; i++) {
+            
+            
+            i = (i+5)-1;
+            
+            System.out.println(i);
+        }
+        
+    }
+}
